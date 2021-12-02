c4a948d75992e7afbe77fe0d893c5dfe026b8976
Mogakamo <mogaka.amo254@gmail.com>
2021-12-02 17:25:21 +0300

    commit

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,33 +1,14 @@
-#ifndef _BINARY_TREES_H_
-#define _BINARY_TREES_H_
-
-#include <stdio.h>
-#include <stdlib.h>
+#include "binary_trees.h"
 
 /**
- * struct binary_tree_s - Binary tree node
+ * binary_tree_is_root - checks if a given node is a root
+ * @node: pointer to the node to check
  *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
+ * Return: 1 if node is a root, and 0 otherwise. If node is NULL, return 0
  */
-typedef struct binary_tree_s
+int binary_tree_is_root(const binary_tree_t *node)
 {
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
-} binary_tree_t;
-
-void binary_tree_print(const binary_tree_t *);
-
-/* tasks */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
-void binary_tree_delete(binary_tree_t *tree);
-int binary_tree_is_leaf(const binary_tree_t *node);
-int binary_tree_is_root(const binary_tree_t *node);
-
-#endif /* _BINARY_TREES_H_ */
+	if (node != NULL && node->parent == NULL)
+		return (1);
+	return (0);
+}
